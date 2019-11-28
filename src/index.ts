934be6a19eb0902d0ae296cdec108c60f6f731a3
Marius Joe <marius.joe@outlook.com>
2019-11-28 17:55:41 +0100

    add basic API config

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import {GitHubStatsApplication} from './application';
+import {ApplicationConfig} from '@loopback/core';
+
+export {GitHubStatsApplication};
+
+export async function main(options: ApplicationConfig = {}) {
+  const app = new GitHubStatsApplication(options);
+  await app.boot();
+  await app.start();
+
+  const url = app.restServer.url;
+  console.log(`Server is running at ${url}`);
+  console.log(`Try ${url}/ping`);
+
+  return app;
+}
---
+++ src/models/user.model.ts
@@ -0,0 +1,34 @@
+import {Model, model, property} from '@loopback/repository';
+
+@model({settings: {strict: false}})
+export class User extends Model {
+  @property({
+    type: 'string',
+    id: true,
+    generated: false,
+    required: true,
+  })
+  name: string;
+
+  @property({
+    type: 'array',
+    itemType: 'object',
+  })
+  repositories?: object[];
+
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+  constructor(data?: Partial<User>) {
+    super(data);
+  }
+}
+
+export interface UserRelations {
+  // describe navigational properties here
+}
+
+export type UserWithRelations = User & UserRelations;
