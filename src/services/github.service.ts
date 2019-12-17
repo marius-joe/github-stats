36309982dce1ec8dda68893c2f373fff5fc0d865
Marius Joe <marius.joe@outlook.com>
2019-12-17 12:26:35 +0100

    add comments for GitHub datasource

--- src/services/github.service.ts
+++ src/services/github.service.ts
@@ -2,6 +2,19 @@ import { getService } from '@loopback/service-proxy'
 import { inject, Provider } from '@loopback/core'
 import { GithubDataSource } from '../datasources'
 
+/**
+ * A REST Service to define methods that map to the operations for the GitHub API
+ */
+export interface GitHubService {
+    // map Node.js method to REST operation as stated in the GitHub datasource file
+    getUser(username: string): Promise<UserGitHub>
+    getRepositories(username: string): Promise<RepoGitHub[]>
+    getBranches(username: string, repository: string): Promise<BranchGitHub[]>
+}
+
+/**
+ * A GitHub User
+ */
 export interface UserGitHub {
     login: string
     name: string
@@ -12,25 +25,27 @@ export interface UserGitHub {
     [x: string]: any
 }
 
+/**
+ * A GitHub User's Repository
+ */
 export interface RepoGitHub {
     name: string
     owner: { login: string }
     [x: string]: any
 }
 
+/**
+ * A GitHub User's Repository Branch
+ */
 export interface BranchGitHub {
     name: string
     commit: { sha: string }
     [x: string]: any
 }
 
-export interface GitHubService {
-    // map Node.js method to REST operation as stated in the GitHub datasource file
-    getUser(username: string): Promise<UserGitHub>
-    getRepositories(username: string): Promise<RepoGitHub[]>
-    getBranches(username: string, repository: string): Promise<BranchGitHub[]>
-}
-
+/**
+ * A REST Service Provider to connect to the GitHub API
+ */
 export class GitHubServiceProvider implements Provider<GitHubService> {
     constructor(
         @inject('datasources.github')
@@ -41,3 +56,11 @@ export class GitHubServiceProvider implements Provider<GitHubService> {
         return getService(this.dataSource)
     }
 }
+
+/**
+ * A generic service interface with any number of methods that return a promise
+ * Can be uses for more general purposes than my GitHub Proxy Service
+ */
+// export interface GenericService {
+//     [methodName: string]: (...args: any[]) => Promise<any>
+// }
